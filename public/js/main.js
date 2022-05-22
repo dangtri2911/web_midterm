bf0714005c6ae4e56f83eceb7174af48c02cccab
Thinh Truong <Thinhtruong2001@yahoo.com>
2022-05-22 11:47:59 +0700

    update tí

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 const { username, room } = Qs.parse(location.search, {
-  ignoreQueryPrefix: true,
+    ignoreQueryPrefix: true,
 });
 
 const chatForm = document.getElementById("chat-form");
@@ -10,24 +10,24 @@ const socket = io();
 socket.emit("joinRoom", { username, room });
 
 socket.on("roomUsers", ({ room, users }) => {
-  createUsers(users);
+    createUsers(users);
 });
 
 socket.on("message", (message) => {
-  createMessage(message);
-  chatMessages.scrollTop = chatMessages.scrollHeight;
+    createMessage(message);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 function createMessage(message) {
-  const div = document.createElement("div");
-  let htmlStr =
-    message.username === "Bot" ? buildBotMsg(message) : buildUserMsg(message);
-  div.innerHTML = htmlStr;
-  document.querySelector(".messages-container").appendChild(div);
+    const div = document.createElement("div");
+    let htmlStr =
+        message.username === "Bot" ? buildBotMsg(message) : buildUserMsg(message);
+    div.innerHTML = htmlStr;
+    document.querySelector(".messages-container").appendChild(div);
 }
 
 function buildBotMsg(message) {
-  return `<div class="row msg-container bot-msg">
+    return `<div class="row msg-container bot-msg">
             <div class="message">
               <p class="text">${message.text}</p>
             </div>
@@ -35,8 +35,8 @@ function buildBotMsg(message) {
 }
 
 function buildUserMsg(message) {
-  const classes = username === message.username ? "current-user-msg" : "";
-  return `<div class="row msg-container ${classes}">
+    const classes = username === message.username ? "current-user-msg" : "";
+    return `<div class="row msg-container ${classes}">
             <div class="message">
             <p class="meta">${message.username} - <span>${message.time}</span></p>
               <p class="text">${message.text}</p>
@@ -45,33 +45,33 @@ function buildUserMsg(message) {
 }
 
 function createUsers(users) {
-  userList.innerHTML = "";
-  const addedUser = [];
-  users.forEach((user) => {
-    if (addedUser.indexOf(user.username) == -1) {
-      const li = document.createElement("li");
-      li.innerText = user.username;
-      userList.appendChild(li);
-      addedUser.push(user.username);
-    }
-  });
+    userList.innerHTML = "";
+    const addedUser = [];
+    users.forEach((user) => {
+        if (addedUser.indexOf(user.username) == -1) {
+            const li = document.createElement("li");
+            li.innerText = user.username;
+            userList.appendChild(li);
+            addedUser.push(user.username);
+        }
+    });
 }
 
 document.getElementById("leave-btn").addEventListener("click", () => {
-  const leaveRoom = confirm("Are you sure you want to leave the chatroom?");
-  if (leaveRoom) {
-    window.location = "/logout";
-  }
+    const leaveRoom = confirm("Are you sure you want to leave the chatroom?");
+    if (leaveRoom) {
+        window.location = "/logout";
+    }
 });
 
 chatForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let msg = e.target.elements.msg.value;
-  msg = msg.trim();
-  if (!msg) {
-    return false;
-  }
-  socket.emit("chatMessage", msg);
-  e.target.elements.msg.value = "";
-  e.target.elements.msg.focus();
+    e.preventDefault();
+    let msg = e.target.elements.msg.value;
+    msg = msg.trim();
+    if (!msg) {
+        return false;
+    }
+    socket.emit("chatMessage", msg);
+    e.target.elements.msg.value = "";
+    e.target.elements.msg.focus();
 });
--- server.js
+++ server.js
@@ -8,11 +8,11 @@ var bodyParser = require("body-parser");
 
 require("dotenv").config();
 const {
-  userJoin,
-  getCurrentUser,
-  leaveRoom,
-  getListUser,
-  usersCredentail,
+    userJoin,
+    getCurrentUser,
+    leaveRoom,
+    getListUser,
+    usersCredentail,
 } = require("./utils/users");
 const { generateToken, getCurrentUserdata } = require("./utils");
 
@@ -23,20 +23,19 @@ const BOT_NAME = "Bot";
 const fileUpload = require("express-fileupload");
 
 app.use((req, res, next) => {
-  if (req.url === "/chat.html") {
-    if (
-      !req.cookies ||
-      !req.cookies[process.env.TOKEN] ||
-      getCurrentUserdata(req) === null
-    ) {
-      res.clearCookie(process.env.TOKEN);
-      res.redirect("/");
-      return;
+    if (req.url === "/chat.html") {
+        if (!req.cookies ||
+            !req.cookies[process.env.TOKEN] ||
+            getCurrentUserdata(req) === null
+        ) {
+            res.clearCookie(process.env.TOKEN);
+            res.redirect("/");
+            return;
+        }
     }
-  }
 
-  next();
-  return;
+    next();
+    return;
 });
 
 // for parsing application/json
@@ -49,76 +48,71 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
 app.get("/user-list", (req, res) => {
-  res.type("json");
-  res.json(Object.keys(usersCredentail));
+    res.type("json");
+    res.json(Object.keys(usersCredentail));
 });
 
 app.get("/logout", (req, res) => {
-  res.clearCookie(process.env.TOKEN);
-  res.redirect("/");
+    res.clearCookie(process.env.TOKEN);
+    res.redirect("/");
 });
 
 app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  if (usersCredentail[username] && usersCredentail[username] == password) {
-    res.cookie(process.env.TOKEN_NAME, generateToken(username));
-    res.json({
-      message: "SUCCESS",
-    });
-    return;
-  } else {
-    res.statusCode = 401;
-    res.json({
-      message: "INVALID_CREDENTIALS",
-    });
-    return;
-  }
-});
-
-app.get("/demo", (req, res) => {
-  res.json(usersCredentail);
-  return;
+    const { username, password } = req.body;
+    if (usersCredentail[username] && usersCredentail[username] == password) {
+        res.cookie(process.env.TOKEN_NAME, generateToken(username));
+        res.json({
+            message: "SUCCESS",
+        });
+        return;
+    } else {
+        res.statusCode = 401;
+        res.json({
+            message: "INVALID_CREDENTIALS",
+        });
+        return;
+    }
 });
 
 io.on("connection", (socket) => {
-  socket.on("joinRoom", ({ username, room }) => {
-    const user = userJoin(socket.id, username, room);
-    socket.join(user.room);
-    socket.emit("message", formatMessage(BOT_NAME, `${username} has joined`));
-    socket.broadcast
-      .to(user.room)
-      .emit(
-        "message",
-        formatMessage(BOT_NAME, `${user.username} has joined the chat`)
-      );
-    io.to(user.room).emit("roomUsers", {
-      room: user.room,
-      users: getListUser(user.room),
+    socket.on("joinRoom", ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
+        socket.join(user.room);
+        socket.emit("message", formatMessage(BOT_NAME, `${username} has joined`));
+        socket.broadcast
+            .to(user.room)
+            .emit(
+                "message",
+                formatMessage(BOT_NAME, `${user.username} has joined the chat`)
+            );
+        io.to(user.room).emit("roomUsers", {
+            room: user.room,
+            users: getListUser(user.room),
+        });
     });
-  });
 
-  socket.on("chatMessage", (msg) => {
-    const user = getCurrentUser(socket.id);
-    io.to(user.room).emit("message", formatMessage(user.username, msg));
-  });
+    socket.on("chatMessage", (msg) => {
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit("message", formatMessage(user.username, msg));
+    });
 
-  socket.on("disconnect", () => {
-    const user = leaveRoom(socket.id);
-    if (user) {
-      io.to(user.room).emit(
-        "message",
-        formatMessage(BOT_NAME, `${user.username} has left the chat`)
-      );
-      io.to(user.room).emit("roomUsers", {
-        room: user.room,
-        users: getListUser(user.room),
-      });
-    }
-  });
+    socket.on("disconnect", () => {
+        const user = leaveRoom(socket.id);
+        if (user) {
+            io.to(user.room).emit(
+                "message",
+                formatMessage(BOT_NAME, `${user.username} has left the chat`)
+            );
+            io.to(user.room).emit("roomUsers", {
+                room: user.room,
+                users: getListUser(user.room),
+            });
+        }
+    });
 });
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () =>
-  console.log(`Application is running on http://localhost:${PORT}`)
+    console.log(`Application is running on http://localhost:${PORT}`)
 );
